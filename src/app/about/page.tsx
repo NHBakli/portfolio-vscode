4ed6b00ce9f4452dd90fb70605f06c3614d21064
NHBakli <bakli25@protonmail.com>
2024-02-15 23:20:15 +0100

    réalisation de la page home + about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import "../../style/about.css";
+import Image from "next/image";
 
-type Props = {};
-
-export default function page({}: Props) {
-  return <div>About it</div>;
+export default function AboutPage() {
+  return (
+    <div className="container">
+      <div className="containertext">
+        <div className="textWithImage">
+          <div className="textContainer">
+            <h3>
+              My name is Bakli, I'm 19 years old, and I'm currently searching
+              for an apprenticeship in Paris or Lille.
+            </h3>
+            <h3>
+              I'm interested in the world of development in general, and I
+              started learning development with Python.{" "}
+            </h3>
+            <h3>
+              During my free time, I continue to enhance my skills by learning
+              new languages or frameworks on my own.
+            </h3>
+            <h3>
+              I fell in love with programming in 2020, and I would like to
+              pursue an apprenticeship because it would allow me to gain
+              experience and skills while advancing in my studies.
+            </h3>
+          </div>
+          <Image
+            src="/bakli.png"
+            alt="bakli"
+            width={300}
+            height={300}
+            className="bakli"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div></div>;
+import Link from "next/link";
+import "../style/home.css";
+
+export default function HomePage() {
+  return (
+    <div className="container">
+      <div className="background">
+        <h1>
+          French <br />
+          Developer
+        </h1>
+      </div>
+      <div className="foreground">
+        <div className="containerInfos">
+          <h1 className="name">NHBakli</h1>
+          <h6 className="bio">Full Stack Web Developer</h6>
+          <Link href="projects">
+            <button className="button">View Work</button>
+          </Link>
+          <Link href="contact">
+            <button className="outlined">Contact Me</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
